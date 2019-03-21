0c5c923c862ebb64d380eada9ced7eac48a081c8
Danny Berkelaar <dannyberkelaar@gmail.com>
2019-03-21 16:55:45 +0100

    add customers page

--- frontend/src/containers/Customers.tsx
+++ frontend/src/containers/Customers.tsx
@@ -1,9 +1,60 @@
+import { gql } from 'apollo-boost';
 import React from 'react';
+import { useQuery } from 'react-apollo-hooks';
 
 import { Content } from '../components/Content';
+import { Table } from '../components/Table';
+import { useRouter } from '../hooks/useRouter';
+import { Identifiable } from '../types';
+
+import { Loading } from './Loading';
+
+type Customers = Identifiable & {
+  id: number,
+  contact: {
+    first_name: string,
+    last_name: string,
+    company: string,
+    telephone: string,
+    website: string
+  }
+};
 
 export function Customers() {
+  const { history } = useRouter();
+  const { loading, data } = useQuery<{
+    users: Customers[]
+  }>(gql`
+    query Users {
+      users {
+        id
+        contact {
+          first_name
+          last_name
+          company
+          telephone
+          website
+        }
+      }
+    }
+  `);
+
   return (
-    <Content title="Klanten"/>
+    <Content title="Klanten">
+      {!loading && data ? (
+        <Table<Customers>
+          rows={data.users}
+          columns={{
+            contact: {
+                heading: 'Naam',
+                render: (value) => `${value.company} - ${value.first_name} ${value.last_name}`
+            }
+          }}
+          onClick={({ id }) => history.push(`/customers/${id}`)}
+        />
+      ) : (
+          <Loading />
+        )}
+    </Content>
   );
 }

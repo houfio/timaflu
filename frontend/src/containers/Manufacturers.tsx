cf9306acc8ac6d4491e4f9eb7290013a4222cb48
Danny Berkelaar <dannyberkelaar@gmail.com>
2019-03-21 20:02:42 +0100

    add manufacturers page

--- frontend/src/containers/Manufacturers.tsx
+++ frontend/src/containers/Manufacturers.tsx
@@ -1,9 +1,71 @@
+import { gql } from 'apollo-boost';
 import React from 'react';
+import { useQuery } from 'react-apollo-hooks';
 
 import { Content } from '../components/Content';
+import { Table } from '../components/Table';
+import { useRouter } from '../hooks/useRouter';
+import { Identifiable } from '../types';
+
+import { Loading } from './Loading';
+
+type Manufacturers = Identifiable & {
+  id: number,
+  contact: {
+    first_name: string,
+    last_name: string,
+    company: string,
+    telephone: string,
+    website: string
+  },
+  products: {
+    id: number,
+    length: number // @LEX FIX DIT
+  }
+};
 
 export function Manufacturers() {
+  const { history } = useRouter();
+  const { loading, data } = useQuery<{
+    manufacturers: Manufacturers[]
+  }>(gql`
+    query Manufacturers {
+      manufacturers {
+        contact {
+          first_name
+          last_name
+          company
+        }
+        products {
+          id
+        }
+      }
+    }
+  `);
+
   return (
-    <Content title="Fabrikanten"/>
+    <Content title="Fabrikanten">
+      {!loading && data ? (
+        <Table<Manufacturers>
+          rows={data.manufacturers}
+          columns={{
+            products: [{
+              heading: 'Aantal producten',
+              render: (value) => value.length
+            }],
+            contact: [{
+              heading: 'Naam',
+              render: (value) => value.company
+            }, {
+              heading: 'Contactpersoon',
+              render: (value) => `${value.first_name} ${value.last_name}`
+            }]
+          }}
+          onClick={({ id }) => history.push(`/manufacturers/${id}`)}
+        />
+      ) : (
+          <Loading />
+        )}
+    </Content>
   );
 }

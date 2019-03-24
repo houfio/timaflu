90fd4ff8f14c5713c1503347d512f5544aae3f5a
houfio <houfio@users.noreply.github.com>
2019-03-24 16:01:32 +0100

    Fix compilation errors

--- frontend/src/containers/Dashboard.tsx
+++ frontend/src/containers/Dashboard.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Line, Bar } from 'react-chartjs-2';
 import { gql } from 'apollo-boost';
+import React from 'react';
 import { Query } from 'react-apollo';
+import { Bar } from 'react-chartjs-2';
 
 import { Content } from '../components/Content';
-import { Breakpoint, Identifiable, InvoiceState } from '../types';
+import { Loading } from '../components/Loading';
+import { Identifiable } from '../types';
 
 type Product = Identifiable & {
-  revenue: string,
+  revenue: number,
   product: {
     name: string
   }
 };
 
-const productRevenueQuery = gql`
-  query Product {
+const query = gql`
+  query Dashboard {
     productRevenue(top: 5) {
       revenue
       product {
@@ -26,45 +27,37 @@ const productRevenueQuery = gql`
 
 export function Dashboard() {
   return (
-    <Content title="Dashboard">
-
-      <h2>Top 5 meest verkochte producten</h2>
-
-      <h2>Inkoop totaal per leverancier</h2>
-      <h2>Welke orders heeft apotheek x openstaan op dit moment en wat is de status van de betreffende order?</h2>
-      <h2>Geef de omzet van alle apothekers voor het afgelopen jaar en daarmee de korting voor volgend jaar.</h2>
-
-
-
-
-      <div style={{ width: 400 }}>
-        <h3>Top 5 meest winstgevende producten</h3>
-
-        <Query<{ product: Product }>
-          query={productRevenueQuery}
-        >
-          {({ loading, data }) => {
-            if (loading) { return 'Laden..' }
-
-            let labels = [];
-            let datasets = [];
-
-            data.forEach(e => {
-              labels.push(e.product.name);
-              datasets.push({ label: e.product.name, data: [e.revenue] })
-            });
-
-            return (
-              <Bar
-                data={{
-                  labels: labels,
-                  datasets: datasets
-                }}
-              />
-            )
-          }}
-        </Query>
-      </div>
-    </Content>
+    <Query<{ productRevenue: Product[] }> query={query}>
+      {({ loading, data }) => (
+        <Content title="Dashboard">
+          {!loading && data ? (
+            <>
+              <h2>Top 5 meest verkochte producten</h2>
+              <h2>Inkoop totaal per leverancier</h2>
+              <h2>
+                Welke orders heeft apotheek x openstaan op dit moment en wat is de status van de betreffende order?
+              </h2>
+              <h2>
+                Geef de omzet van alle apothekers voor het afgelopen jaar en daarmee de korting voor volgend jaar.
+              </h2>
+              <div style={{ width: 400 }}>
+                <h3>Top 5 meest winstgevende producten</h3>
+                <Bar
+                  data={{
+                    labels: data.productRevenue.map((revenue) => revenue.product.name),
+                    datasets: data.productRevenue.map((revenue) => ({
+                      label: revenue.product.name,
+                      data: [revenue.revenue]
+                    }))
+                  }}
+                />
+              </div>
+            </>
+          ) : (
+            <Loading/>
+          )}
+        </Content>
+      )}
+    </Query>
   );
 }
